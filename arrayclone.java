a2b9d1c98976beb418399de49aae0fe2e4000f19
DHAVALHADIYA <hadiyadhaval5@gmail.com>
2023-09-19 19:30:20 +0530

    difference and diff lang is added

---
+++ arrayclone.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class arrayclone {
+    public static void main(String[] args) {
+        HashSet<String> hash = new HashSet<>();
+        // hash.clear();
+        for (String string : hash) {
+            System.out.println(string);
+        }
+
+    }
+}
