bfd10fa924902092834bb75237c76a21585bfb9d
DHAVALHADIYA <hadiyadhaval5@gmail.com>
2023-09-27 12:47:07 +0530

    changes in cn file and some program

---
+++ Practices 22_9/collection.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class collection {
+    public static void main(String[] args) {
+        // ArrayList<String> list = new ArrayList<String>();
+        // list.add("shshs");
+        // list.add("sssjjjk");
+        // Collections.unmodifiableList(list);
+        // list.add("ssksk");
+        // System.out.println(list);
+
+        HashMap<Integer , Integer> hash = new HashMap<>();
+        hash.put(null, 4);
+        hash.put(null, 42);
+        hash.put(null, 5);
+        System.out.println(hash);
+
+        // try {
+        //     int a[] = { 1, 2, 3, 4 };
+        //     for (int i = 1; i <= a.length; i++) {
+        //         System.out.println(a[i]);
+        //     }
+        // } catch (Exception e) {
+        //     // TODO: handle exception
+        //     System.out.println(e);
+        // } 
+
+    }
+}
---
+++ Practices 22_9/eception.java
@@ -0,0 +1,30 @@
+// class base extends Exception {
+// }
+
+// class derive extends base {
+// }
+
+// class a {
+//      int a ;
+// }
+
+public class eception {
+    public static void main(String[] args) {
+        // try {
+        // throw new derive();
+        // }
+        // catch (base e) {
+        // // TODO: handle exception
+        // System.out.println("base");
+        // }
+
+        // a ab = new a();
+
+        // System.out.println(ab.a);
+          String s = "daa";
+          s.concat("ss");
+          System.out.println(s);
+
+    }
+
+}
